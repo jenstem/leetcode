a706601723618975b3c72aee38e8c08bc6f63d28
jenstem <jennifer.stemkowski@gmail.com>
2024-10-17 16:46:21 -0400

    Problem No. 83 Remove Duplicates from Sorted List

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,20 @@
-# The Tribonacci sequence Tn is defined as follows:
+# Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
 
-# T0 = 0, T1 = 1, T2 = 1, and Tn+3 = Tn + Tn+1 + Tn+2 for n >= 0.
-
-# Given n, return the value of Tn.
-
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
-    def tribonacci(self, n):
+    def deleteDuplicates(self, head):
         """
-        :type n: int
-        :rtype: int
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
         """
-        if n == 0:
-            return 0
-        elif n == 1 or n == 2:
-            return 1
-
-        t0, t1, t2 = 0, 1, 1
-        for i in range(3, n + 1):
-            t_next = t0 + t1 + t2
-            t0, t1, t2 = t1, t2, t_next
-
-        return t2
+        current = head
+        while current and current.next:
+            if current.val == current.next.val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return head
