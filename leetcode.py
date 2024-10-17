8f642cd84cce6804d418cf53dde36289312b0388
jenstem <jennifer.stemkowski@gmail.com>
2024-10-17 13:52:27 -0400

    Problem No. 746 Min Cost Climbing Stairs

--- leetcode.py
+++ leetcode.py
@@ -1,17 +1,19 @@
-# You are climbing a staircase. It takes n steps to reach the top.
+# You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
 
-# Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+# You can either start from the step with index 0, or the step with index 1.
+
+# Return the minimum cost to reach the top of the floor.
 
 class Solution(object):
-    def climbStairs(self, n):
+    def minCostClimbingStairs(self, cost):
         """
-        :type n: int
+        :type cost: List[int]
         :rtype: int
         """
-        if n <= 1:
-            return 1
-        ways = [0] * (n + 1)
-        ways[0], ways[1] = 1, 1
+        n = len(cost)
+        dp = [0] * (n + 1)
+
         for i in range(2, n + 1):
-            ways[i] = ways[i - 1] + ways[i - 2]
-        return ways[n]
+            dp[i] = min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2])
+
+        return dp[n]
