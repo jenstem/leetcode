8a2d747632155f1df3865f3aec274e388e6318de
jenstem <jennifer.stemkowski@gmail.com>
2024-10-17 11:47:37 -0400

    Problem No. 67 Add Binary

--- leetcode.py
+++ leetcode.py
@@ -1,16 +1,4 @@
-# You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
+# Given two binary strings a and b, return their sum as a binary string.
 
-# Increment the large integer by one and return the resulting array of digits.
-
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        for i in range(len(digits) - 1, -1, -1):
-            if digits[i] < 9:
-                digits[i] += 1
-                return digits
-            digits[i] = 0
-        return [1] + digits
+def addBinary(self, a, b):
+    return bin(int(a, 2) + int(b, 2))[2:]
