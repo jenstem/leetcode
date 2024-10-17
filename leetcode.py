9960c9119da85494b6e0e2049bf73121ba05d14e
jenstem <jennifer.stemkowski@gmail.com>
2024-10-17 11:58:04 -0400

    Problem No. 989 Add to Array-Form of Integer

--- leetcode.py
+++ leetcode.py
@@ -1,4 +1,15 @@
-# Given two binary strings a and b, return their sum as a binary string.
+# The array-form of an integer num is an array representing its digits in left to right order.
 
-def addBinary(self, a, b):
-    return bin(int(a, 2) + int(b, 2))[2:]
+# For example, for num = 1321, the array form is [1,3,2,1].
+# Given num, the array-form of an integer, and an integer k, return the array-form of the integer num + k.
+
+class Solution(object):
+    def addToArrayForm(self, num, k):
+        """
+        :type num: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        num_str = ''.join(map(str, num))
+        total = int(num_str) + k
+        return [int(digit) for digit in str(total)]
