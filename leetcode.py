c7f5aa931ba8252d6de44a32eba0e17d00061aa9
jenstem <jennifer.stemkowski@gmail.com>
2024-10-18 10:21:00 +0100

    Problem No. 101 Symmetric Tree

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,3 @@
-# Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+# Given the root of a binary tree, return its maximum depth.
 
-# Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
-
-
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution(object):
-    def isSameTree(self, p, q):
-        """
-        :type p: Optional[TreeNode]
-        :type q: Optional[TreeNode]
-        :rtype: bool
-        """
-        if p == q:
-            return True
-        if p is None or q is None or p.val != q.val:
-            return False
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
