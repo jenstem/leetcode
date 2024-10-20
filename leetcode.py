605c47e64c14a32b98e38e21e44d23fab42dbe41
jenstem <jennifer.stemkowski@gmail.com>
2024-10-20 14:49:00 +0100

    Problem No. 110 Balanced Binary Tree

--- leetcode.py
+++ leetcode.py
@@ -1,6 +1,4 @@
-# Given the root of a binary tree, return its maximum depth.
-
-# A binary tree's maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+# Given a binary tree, determine if it is height-balanced
 
 # Definition for a binary tree node.
 # class TreeNode(object):
@@ -9,14 +7,22 @@
 #         self.left = left
 #         self.right = right
 class Solution(object):
-    def maxDepth(self, root):
+    def isBalanced(self, root):
         """
         :type root: Optional[TreeNode]
-        :rtype: int
+        :rtype: bool
         """
-        if not root:
-            return 0
-        else:
-            left_depth = self.maxDepth(root.left)
-            right_depth = self.maxDepth(root.right)
-            return 1 + max(left_depth, right_depth)
+        def check_height(node):
+            if not node:
+                return 0
+            left_height = check_height(node.left)
+            if left_height == -1:
+                return -1
+            right_height = check_height(node.right)
+            if right_height == -1:
+                return -1
+            if abs(left_height - right_height) > 1:
+                return -1
+            return max(left_height, right_height) + 1
+
+        return check_height(root) != -1
