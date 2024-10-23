cafaa321edf4e176e29218b12120f6a90ea8211e
jenstem <jennifer.stemkowski@gmail.com>
2024-10-23 14:46:13 -0400

    Problem No. 119 Pascal's Triangle II

--- leetcode.py
+++ leetcode.py
@@ -1,17 +1,14 @@
-# Given an integer numRows, return the first numRows of Pascal's triangle.
+# Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
 
 # In Pascal's triangle, each number is the sum of the two numbers directly above it
 
 class Solution(object):
-    def generate(self, numRows):
+    def getRow(self, rowIndex):
         """
-        :type numRows: int
-        :rtype: List[List[int]]
+        :type rowIndex: int
+        :rtype: List[int]
         """
-        triangle = []
-        for row_num in range(numRows):
-            row = [1] * (row_num + 1)
-            for j in range(1, row_num):
-                row[j] = triangle[row_num - 1][j - 1] + triangle[row_num - 1][j]
-            triangle.append(row)
-        return triangle
+        row = [1]
+        for _ in range(rowIndex):
+            row.append(row[-1] * (rowIndex - _) // (_ + 1))
+        return row
