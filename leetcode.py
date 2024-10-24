11be4f2713c92c273d96891666d5d099fd0e5279
jenstem <jennifer.stemkowski@gmail.com>
2024-10-24 11:20:12 -0400

    Problem No. 141 Linked List Cycle

--- leetcode.py
+++ leetcode.py
@@ -1,14 +1,26 @@
-# Given an array nums containing n distinct numbers in the range [0, n],
-# return the only number in the range that is missing from the array.
+# Given head, the head of a linked list, determine if the linked list has a cycle in it.
 
+# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
+
+# Return true if there is a cycle in the linked list. Otherwise, return false.
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
 
 class Solution(object):
-    def missingNumber(self, nums):
+    def hasCycle(self, head):
         """
-        :type nums: List[int]
-        :rtype: int
+        :type head: ListNode
+        :rtype: bool
         """
-        n = len(nums)
-        expected_sum = n * (n + 1) // 2
-        actual_sum = sum(nums)
-        return expected_sum - actual_sum
+        slow = fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
