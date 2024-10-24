d212cd72613cd378c71dd8ab2ba622cbe45352b4
jenstem <jennifer.stemkowski@gmail.com>
2024-10-24 10:50:25 -0400

    Problem No. 136 Single Number

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,15 @@
-# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
 
-# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
 
-# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
 class Solution(object):
-    def isPalindrome(self, s):
+    def singleNumber(self, nums):
         """
-        :type s: str
-        :rtype: bool
+        :type nums: List[int]
+        :rtype: int
         """
-        i, j = 0, len(s) - 1
-        while i < j:
-            if not s[i].isalnum():
-                i += 1
-            elif not s[j].isalnum():
-                j -= 1
-            elif s[i].lower() != s[j].lower():
-                return False
-            else:
-                i, j = i + 1, j - 1
-        return True
+        result = 0
+        for num in nums:
+            result ^= num
+        return result
