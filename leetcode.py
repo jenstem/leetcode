6d295851a577ac6b91233b89d3221e2ffaf94169
jenstem <jennifer.stemkowski@gmail.com>
2024-10-25 18:11:00 +0100

    Problem No. 144 Binary Tree Preorder Traversal

--- leetcode.py
+++ leetcode.py
@@ -1,26 +1,24 @@
-# Given head, the head of a linked list, determine if the linked list has a cycle in it.
+# Given the root of a binary tree, return the preorder traversal of its nodes' values.
 
-# There is a cycle in a linked list if there is some node in the list that can be reached again by continuously following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that pos is not passed as a parameter.
-
-# Return true if there is a cycle in the linked list. Otherwise, return false.
-
-
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution(object):
-    def hasCycle(self, head):
+    def preorderTraversal(self, root):
         """
-        :type head: ListNode
-        :rtype: bool
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
         """
-        slow = fast = head
-        while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-            if slow == fast:
-                return True
-        return False
+        result = []
+
+        def traverse(node):
+            if node:
+                result.append(node.val)
+                traverse(node.left)
+                traverse(node.right)
+
+        traverse(root)
+        return result
