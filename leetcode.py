e23d8929ad2b88dec585080e4033e877394ff86a
jenstem <jennifer.stemkowski@gmail.com>
2024-10-27 10:29:00 +0100

    Problem No. 168 Excel Sheet Column Title

--- leetcode.py
+++ leetcode.py
@@ -1,17 +1,15 @@
-# Given the heads of two singly linked-lists headA and headB, return the node at which the two lists intersect. If the two linked lists have no intersection at all, return null.
+# Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
 
 class Solution(object):
-    def getIntersectionNode(self, headA, headB):
+    def convertToTitle(self, columnNumber):
         """
-        :type head1, head1: ListNode
-        :rtype: ListNode
+        :type columnNumber: int
+        :rtype: str
         """
-        if not headA or not headB:
-            return None
+        title = ""
 
-        a, b = headA, headB
-        while a != b:
-            a = a.next if a else headB
-            b = b.next if b else headA
-
-        return a
+        while columnNumber > 0:
+            columnNumber -= 1
+            title = chr(columnNumber % 26 + ord('A')) + title
+            columnNumber //= 26
+        return title
