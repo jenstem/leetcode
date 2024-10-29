06a8d736807e4be9f9c8128d156a72435f728a5c
jenstem <jennifer.stemkowski@gmail.com>
2024-10-29 10:07:26 -0400

    Problem No. 171 Excel Sheet Column Number

--- leetcode.py
+++ leetcode.py
@@ -1,19 +1,13 @@
-# Given an array nums of size n, return the majority element.
-
-# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
+# Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
 
 class Solution(object):
-    def majorityElement(self, nums):
+    def titleToNumber(self, columnTitle):
         """
-        :type nums: List[int]
+        :type columnTitle: str
         :rtype: int
         """
-        count = 0
-        candidate = None
+        result = 0
 
-        for num in nums:
-            if count == 0:
-                candidate = num
-            count += (1 if num == candidate else -1)
-
-        return candidate
+        for i, char in enumerate(reversed(columnTitle)):
+            result += (ord(char) - ord('A') + 1) * (26 ** i)
+        return result
