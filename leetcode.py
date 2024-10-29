3d104b60d5bc59bd5fb6dc6a2bc9fd64b0d50e38
jenstem <jennifer.stemkowski@gmail.com>
2024-10-29 09:33:41 -0400

    Problem No. 169 Majority Element

--- leetcode.py
+++ leetcode.py
@@ -1,15 +1,19 @@
-# Given an integer columnNumber, return its corresponding column title as it appears in an Excel sheet.
+# Given an array nums of size n, return the majority element.
+
+# The majority element is the element that appears more than ⌊n / 2⌋ times. You may assume that the majority element always exists in the array.
 
 class Solution(object):
-    def convertToTitle(self, columnNumber):
+    def majorityElement(self, nums):
         """
-        :type columnNumber: int
-        :rtype: str
+        :type nums: List[int]
+        :rtype: int
         """
-        title = ""
+        count = 0
+        candidate = None
 
-        while columnNumber > 0:
-            columnNumber -= 1
-            title = chr(columnNumber % 26 + ord('A')) + title
-            columnNumber //= 26
-        return title
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
