4255038efa9793fedd618e0d1f34888c1cd2185a
jenstem <jennifer.stemkowski@gmail.com>
2024-10-30 09:21:57 -0400

    Problem No. 203 Remove Linked List Elements

--- leetcode.py
+++ leetcode.py
@@ -1,13 +1,23 @@
-# Given a string columnTitle that represents the column title as appears in an Excel sheet, return its corresponding column number.
+# Given the head of a linked list and an integer val, remove all the nodes of the linked list that has Node.val == val, and return the new head.
 
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution(object):
-    def titleToNumber(self, columnTitle):
+    def removeElements(self, head, val):
         """
-        :type columnTitle: str
-        :rtype: int
+        :type head: Optional[ListNode]
+        :type val: int
+        :rtype: Optional[ListNode]
         """
-        result = 0
-
-        for i, char in enumerate(reversed(columnTitle)):
-            result += (ord(char) - ord('A') + 1) * (26 ** i)
-        return result
+        dummy = ListNode(0)
+        dummy.next = head
+        current = dummy
+        while current.next:
+            if current.next.val == val:
+                current.next = current.next.next
+            else:
+                current = current.next
+        return dummy.next
