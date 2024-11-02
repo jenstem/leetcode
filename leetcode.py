3513dc9c5c5b6d620046705ff3fd1331f68f5531
jenstem <jennifer.stemkowski@gmail.com>
2024-11-02 08:51:00 +0100

    Problem No. 219 Contains Duplicate II

--- leetcode.py
+++ leetcode.py
@@ -1,9 +1,15 @@
-# Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+# Given an integer array nums and an integer k, return true if there are two distinct indices i and j in the array such that nums[i] == nums[j] and abs(i - j) <= k.
 
 class Solution(object):
-    def containsDuplicate(self, nums):
+    def containsNearbyDuplicate(self, nums, k):
         """
         :type nums: List[int]
+        :type k: int
         :rtype: bool
         """
-        return len(nums) != len(set(nums))
+        num_dict = {}
+        for i, num in enumerate(nums):
+            if num in num_dict and abs(i - num_dict[num]) <= k:
+                return True
+            num_dict[num] = i
+        return False
