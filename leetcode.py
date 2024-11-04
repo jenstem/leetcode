19ce123fc03a0c214e5d7f6688172956a91c46d3
jenstem <jennifer.stemkowski@gmail.com>
2024-11-04 09:53:19 -0500

    Problem No. 226 Invert Binary Tree

--- leetcode.py
+++ leetcode.py
@@ -1,52 +1,23 @@
-# Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+# Given the root of a binary tree, invert the tree, and return its root.
 
-# Implement the MyStack class:
-
-# void push(int x) Pushes element x to the top of the stack.
-# int pop() Removes the element on the top of the stack and returns it.
-# int top() Returns the element on the top of the stack.
-# boolean empty() Returns true if the stack is empty, false otherwise.
-# Notes:
-
-# You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
-# Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
-
-from collections import deque
-
-
-class MyStack(object):
-
-    def __init__(self):
-        self.queue1 = deque()
-        self.queue2 = deque()
-
-
-    def push(self, x):
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def invertTree(self, root):
         """
-        :type x: int
-        :rtype: None
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
         """
-        self.queue2.append(x)
-        while self.queue1:
-            self.queue2.append(self.queue1.popleft())
-        self.queue1, self.queue2 = self.queue2, self.queue1
+        def dfs(root):
+            if root is None:
+                return
+            root.left, root.right = root.right, root.left
+            dfs(root.left)
+            dfs(root.right)
 
-
-    def pop(self):
-        """
-        :rtype: int
-        """
-        return self.queue1.popleft()
-
-    def top(self):
-        """
-        :rtype: int
-        """
-        return self.queue1[0]
-
-
-    def empty(self):
-        """
-        :rtype: bool
-        """
-        return len(self.queue1) == 0
+        dfs(root)
+        return root
