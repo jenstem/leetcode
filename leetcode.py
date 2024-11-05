b715b91a9cc11c2bf90381544c731c580e8a9c10
jenstem <jennifer.stemkowski@gmail.com>
2024-11-05 11:56:19 -0500

    Problem No. 232 Implement Queue using Stacks

--- leetcode.py
+++ leetcode.py
@@ -1,13 +1,61 @@
-# Given an integer n, return true if it is a power of two. Otherwise, return false.
+# Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
 
-# An integer n is a power of two, if there exists an integer x such that n == 2x.
+# Implement the MyQueue class:
 
-class Solution(object):
-    def isPowerOfTwo(self, n):
+# void push(int x) Pushes element x to the back of the queue.
+# int pop() Removes the element from the front of the queue and returns it.
+# int peek() Returns the element at the front of the queue.
+# boolean empty() Returns true if the queue is empty, false otherwise.
+# Notes:
+
+# You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+# Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+
+class MyQueue(object):
+
+
+    def __init__(self):
+        self.stack1 = []
+        self.stack2 = []
+
+
+    def push(self, x):
         """
-        :type n: int
+        :type x: int
+        :rtype: None
+        """
+        self.stack1.append(x)
+
+
+    def pop(self):
+        """
+        :rtype: int
+        """
+        self.peek()
+        return self.stack2.pop()
+
+
+
+    def peek(self):
+        """
+        :rtype: int
+        """
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+
+
+    def empty(self):
+        """
         :rtype: bool
         """
-        if n <= 0:
-            return False
-        return (n & (n - 1)) == 0
+        return not self.stack1 and not self.stack2
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
