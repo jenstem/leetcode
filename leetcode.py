ab416a896e9542e07e65fdf51a0d6d6f843bc9f5
jenstem <jennifer.stemkowski@gmail.com>
2024-11-07 09:45:43 -0500

    Problem No. 278 First Bad Version

--- leetcode.py
+++ leetcode.py
@@ -1,16 +1,26 @@
-# An ugly number is a positive integer whose prime factors are limited to 2, 3, and 5.
+# You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
 
-# Given an integer n, return true if n is an ugly number.
+# Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+# You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
 
 class Solution(object):
-    def isUgly(self, n):
+    def firstBadVersion(self, n):
         """
         :type n: int
-        :rtype: bool
+        :rtype: int
         """
-        if n <= 0:
-            return False
-        for prime in [2,3,5]:
-            while n % prime == 0:
-                n //= prime
-        return n == 1
+        left, right = 1, n
+        while left < right:
+            mid = left + (right - left) // 2
+            if isBadVersion(mid):
+                right = mid
+            else:
+                left = mid + 1
+        return left
