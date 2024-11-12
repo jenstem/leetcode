34979717693c0f78190d2079d2f4af6d8ccd9682
jenstem <jennifer.stemkowski@gmail.com>
2024-11-12 09:13:00 +0100

    Problem No. 182 Duplicate Emails

--- leetcode.py
+++ leetcode.py
@@ -1,36 +1,18 @@
 # +-------------+---------+
 # | Column Name | Type    |
 # +-------------+---------+
-# | empId       | int     |
-# | name        | varchar |
-# | supervisor  | int     |
-# | salary      | int     |
+# | id          | int     |
+# | email       | varchar |
 # +-------------+---------+
-# empId is the column with unique values for this table.
-# Each row of this table indicates the name and the ID of an employee in addition to their salary and the id of their manager.
+# id is the primary key (column with unique values) for this table.
+# Each row of this table contains an email. The emails will not contain uppercase letters.
 
 
-# Table: Bonus
-
-# +-------------+------+
-# | Column Name | Type |
-# +-------------+------+
-# | empId       | int  |
-# | bonus       | int  |
-# +-------------+------+
-# empId is the column of unique values for this table.
-# empId is a foreign key (reference column) to empId from the Employee table.
-# Each row of this table contains the id of an employee and their respective bonus.
-
-
-# Write a solution to report the name and bonus amount of each employee with a bonus less than 1000.
+# Write a solution to report all the duplicate emails. Note that it's guaranteed that the email field is not NULL.
 
 # Return the result table in any order.
 
-# The result format is in the following example.
-
-SELECT name, bonus
-FROM
-    Employee
-    LEFT JOIN Bonus USING (empId)
-WHERE IFNULL(bonus, 0) < 1000;
+SELECT email
+FROM Person
+GROUP BY email
+HAVING COUNT(email) > 1;
