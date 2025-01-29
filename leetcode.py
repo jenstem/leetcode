23e8a94ecdcc0ab98f8d492e724529baac467032
jenstem <jennifer.stemkowski@gmail.com>
2025-01-29 06:48:00 +0100

    Solve Problem No. 463 Island Perimeter

--- leetcode.py
+++ leetcode.py
@@ -1,30 +1,41 @@
-# Hamming Distance #461
+# Island Perimeter #463
 
-# The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+# You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
 
-# Given two integers x and y, return the Hamming distance between them.
+# Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
 
-
+# The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
 
 # Example 1:
-
-# Input: x = 1, y = 4
-# Output: 2
-# Explanation:
-# 1   (0 0 0 1)
-# 4   (0 1 0 0)
-#        ↑   ↑
-# The above arrows point to positions where the corresponding bits are different.
+# Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
+# Output: 16
+# Explanation: The perimeter is the 16 yellow stripes in the image above.
 # Example 2:
 
-# Input: x = 3, y = 1
-# Output: 1
+# Input: grid = [[1]]
+# Output: 4
+# Example 3:
 
+# Input: grid = [[1,0]]
+# Output: 4
 
 # Constraints:
-
-# 0 <= x, y <= 231 - 1
+# row == grid.length
+# col == grid[i].length
+# 1 <= row, col <= 100
+# grid[i][j] is 0 or 1.
+# There is exactly one island in grid.
 
 class Solution:
-    def hammingDistance(self, x: int, y: int) -> int:
-        return (x ^ y).bit_count()
+    def islandPerimeter(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        ans = 0
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    ans += 4
+                    if i < m - 1 and grid[i + 1][j] == 1:
+                        ans -= 2
+                    if j < n - 1 and grid[i][j + 1] == 1:
+                        ans -= 2
+        return ans
