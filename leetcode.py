4c1e555cc46d17bab8a9606c1799c51893a756ca
jenstem <jennifer.stemkowski@gmail.com>
2025-01-30 09:14:33 -0500

    Solve Problem No. 476 Number Complement

--- leetcode.py
+++ leetcode.py
@@ -1,41 +1,28 @@
-# Island Perimeter #463
+# Number Complement #476
 
-# You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+# The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its binary representation.
 
-# Grid cells are connected horizontally/vertically (not diagonally). The grid is completely surrounded by water, and there is exactly one island (i.e., one or more connected land cells).
+# For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+# Given an integer num, return its complement.
 
-# The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island. One cell is a square with side length 1. The grid is rectangular, width and height don't exceed 100. Determine the perimeter of the island.
+
 
 # Example 1:
-# Input: grid = [[0,1,0,0],[1,1,1,0],[0,1,0,0],[1,1,0,0]]
-# Output: 16
-# Explanation: The perimeter is the 16 yellow stripes in the image above.
+
+# Input: num = 5
+# Output: 2
+# Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
 # Example 2:
 
-# Input: grid = [[1]]
-# Output: 4
-# Example 3:
+# Input: num = 1
+# Output: 0
+# Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
 
-# Input: grid = [[1,0]]
-# Output: 4
 
 # Constraints:
-# row == grid.length
-# col == grid[i].length
-# 1 <= row, col <= 100
-# grid[i][j] is 0 or 1.
-# There is exactly one island in grid.
+
+# 1 <= num < 231
 
 class Solution:
-    def islandPerimeter(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        ans = 0
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == 1:
-                    ans += 4
-                    if i < m - 1 and grid[i + 1][j] == 1:
-                        ans -= 2
-                    if j < n - 1 and grid[i][j + 1] == 1:
-                        ans -= 2
-        return ans
+    def findComplement(self, num: int) -> int:
+        return num ^ ((1 << num.bit_length()) - 1)
