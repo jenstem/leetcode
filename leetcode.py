1adb29f2bc3b86f07b26f6ec35c6f8303d4fb786
jenstem <jennifer.stemkowski@gmail.com>
2025-02-03 17:08:23 -0500

    Solve Problem No. 557 Reverse Words in a String III

--- leetcode.py
+++ leetcode.py
@@ -1,40 +1,24 @@
-# Relative Ranks #506
+# Reverse Words in a String III #557
 
-# You are given an integer array score of size n, where score[i] is the score of the ith athlete in a competition. All the scores are guaranteed to be unique.
-
-# The athletes are placed based on their scores, where the 1st place athlete has the highest score, the 2nd place athlete has the 2nd highest score, and so on. The placement of each athlete determines their rank:
-
-# The 1st place athlete's rank is "Gold Medal".
-# The 2nd place athlete's rank is "Silver Medal".
-# The 3rd place athlete's rank is "Bronze Medal".
-# For the 4th place to the nth place athlete, their rank is their placement number (i.e., the xth place athlete's rank is "x").
-# Return an array answer of size n where answer[i] is the rank of the ith athlete.
+# Given a string s, reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
 
 # Example 1:
 
-# Input: score = [5,4,3,2,1]
-# Output: ["Gold Medal","Silver Medal","Bronze Medal","4","5"]
-# Explanation: The placements are [1st, 2nd, 3rd, 4th, 5th].
+# Input: s = "Let's take LeetCode contest"
+# Output: "s'teL ekat edoCteeL tsetnoc"
 # Example 2:
 
-# Input: score = [10,3,8,9,4]
-# Output: ["Gold Medal","5","Bronze Medal","Silver Medal","4"]
-# Explanation: The placements are [1st, 5th, 3rd, 2nd, 4th].
+# Input: s = "Mr Ding"
+# Output: "rM gniD"
 
 # Constraints:
 
-# n == score.length
-# 1 <= n <= 104
-# 0 <= score[i] <= 106
-# All the values in score are unique.
+# 1 <= s.length <= 5 * 104
+# s contains printable ASCII characters.
+# s does not contain any leading or trailing spaces.
+# There is at least one word in s.
+# All the words in s are separated by a single space.
 
 class Solution:
-    def findRelativeRanks(self, score: List[int]) -> List[str]:
-        n = len(score)
-        idx = list(range(n))
-        idx.sort(key=lambda x: -score[x])
-        top3 = ["Gold Medal", "Silver Medal", "Bronze Medal"]
-        ans = [None] * n
-        for i, j in enumerate(idx):
-            ans[j] = top3[i] if i < 3 else str(i + 1)
-        return ans
+    def reverseWords(self, s: str) -> str:
+        return " ".join(t[::-1] for t in s.split())
