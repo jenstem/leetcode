02b91f3a4d3ec920b846e103a1775001b9527273
jenstem <jennifer.stemkowski@gmail.com>
2025-02-05 07:26:00 +0100

    Solve Problem No. 191 Number of 1 Bits

--- leetcode.py
+++ leetcode.py
@@ -1,33 +1,52 @@
-# Reverse Bit #190
+# Number of 1 Bits #191
 
-# Reverse bits of a given 32 bits unsigned integer.
+# Given a positive integer n, write a function that returns the number of 
+# set bits
+#  in its binary representation (also known as the Hamming weight).
 
-# Note:
-
-# Note that in some languages, such as Java, there is no unsigned integer type. In this case, both input and output will be given as a signed integer type. They should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
-# In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 2 above, the input represents the signed integer -3 and the output represents the signed integer -1073741825.
  
 
 # Example 1:
 
-# Input: n = 00000010100101000001111010011100
-# Output:    964176192 (00111001011110000010100101000000)
-# Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.
+# Input: n = 11
+
+# Output: 3
+
+# Explanation:
+
+# The input binary string 1011 has a total of three set bits.
+
 # Example 2:
 
-# Input: n = 11111111111111111111111111111101
-# Output:   3221225471 (10111111111111111111111111111111)
-# Explanation: The input binary string 11111111111111111111111111111101 represents the unsigned integer 4294967293, so return 3221225471 which its binary representation is 10111111111111111111111111111111.
+# Input: n = 128
+
+# Output: 1
+
+# Explanation:
+
+# The input binary string 10000000 has a total of one set bit.
+
+# Example 3:
+
+# Input: n = 2147483645
+
+# Output: 30
+
+# Explanation:
+
+# The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+
  
 
 # Constraints:
 
-# The input must be a binary string of length 32
+# 1 <= n <= 231 - 1
+
 
 class Solution:
-    def reverseBits(self, n: int) -> int:
+    def hammingWeight(self, n: int) -> int:
         ans = 0
-        for i in range(32):
-            ans |= (n & 1) << (31 - i)
-            n >>= 1
+        while n:
+            n &= n - 1
+            ans += 1
         return ans
