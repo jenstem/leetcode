3698d0a345de378c26f246166258e1c976a2e995
jenstem <jennifer.stemkowski@gmail.com>
2025-02-06 07:58:00 +0100

    Solve Problem No. 228 Summary Ranges

--- leetcode.py
+++ leetcode.py
@@ -1,52 +1,55 @@
-# Number of 1 Bits #191
+# Summary Ranges #228
 
-# Given a positive integer n, write a function that returns the number of 
-# set bits
-#  in its binary representation (also known as the Hamming weight).
+# You are given a sorted unique integer array nums.
 
+# A range [a,b] is the set of all integers from a to b (inclusive).
+
+# Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
+
+# Each range [a,b] in the list should be output as:
+
+# "a->b" if a != b
+# "a" if a == b
  
 
 # Example 1:
 
-# Input: n = 11
-
-# Output: 3
-
-# Explanation:
-
-# The input binary string 1011 has a total of three set bits.
-
+# Input: nums = [0,1,2,4,5,7]
+# Output: ["0->2","4->5","7"]
+# Explanation: The ranges are:
+# [0,2] --> "0->2"
+# [4,5] --> "4->5"
+# [7,7] --> "7"
 # Example 2:
 
-# Input: n = 128
-
-# Output: 1
-
-# Explanation:
-
-# The input binary string 10000000 has a total of one set bit.
-
-# Example 3:
-
-# Input: n = 2147483645
-
-# Output: 30
-
-# Explanation:
-
-# The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
-
+# Input: nums = [0,2,3,4,6,8,9]
+# Output: ["0","2->4","6","8->9"]
+# Explanation: The ranges are:
+# [0,0] --> "0"
+# [2,4] --> "2->4"
+# [6,6] --> "6"
+# [8,9] --> "8->9"
  
 
 # Constraints:
 
-# 1 <= n <= 231 - 1
-
+# 0 <= nums.length <= 20
+# -231 <= nums[i] <= 231 - 1
+# All the values of nums are unique.
+# nums is sorted in ascending order.
 
 class Solution:
-    def hammingWeight(self, n: int) -> int:
-        ans = 0
-        while n:
-            n &= n - 1
-            ans += 1
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        def f(i: int, j: int) -> str:
+            return str(nums[i]) if i == j else f'{nums[i]}->{nums[j]}'
+
+        i = 0
+        n = len(nums)
+        ans = []
+        while i < n:
+            j = i
+            while j + 1 < n and nums[j + 1] == nums[j] + 1:
+                j += 1
+            ans.append(f(i, j))
+            i = j + 1
         return ans
