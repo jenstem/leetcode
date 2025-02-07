37bbe21c5f259ad30023e05931ad1d76a5c2747c
jenstem <jennifer.stemkowski@gmail.com>
2025-02-07 06:19:00 +0100

    Solve Problem No. 242 Valid Anagram

--- leetcode.py
+++ leetcode.py
@@ -1,55 +1,37 @@
-# Summary Ranges #228
+# Valid Anagram #242
 
-# You are given a sorted unique integer array nums.
+# Given two strings s and t, return true if t is an 
+# anagram
+#  of s, and false otherwise.
 
-# A range [a,b] is the set of all integers from a to b (inclusive).
-
-# Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is, each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the ranges but not in nums.
-
-# Each range [a,b] in the list should be output as:
-
-# "a->b" if a != b
-# "a" if a == b
  
 
 # Example 1:
 
-# Input: nums = [0,1,2,4,5,7]
-# Output: ["0->2","4->5","7"]
-# Explanation: The ranges are:
-# [0,2] --> "0->2"
-# [4,5] --> "4->5"
-# [7,7] --> "7"
+# Input: s = "anagram", t = "nagaram"
+
+# Output: true
+
 # Example 2:
 
-# Input: nums = [0,2,3,4,6,8,9]
-# Output: ["0","2->4","6","8->9"]
-# Explanation: The ranges are:
-# [0,0] --> "0"
-# [2,4] --> "2->4"
-# [6,6] --> "6"
-# [8,9] --> "8->9"
+# Input: s = "rat", t = "car"
+
+# Output: false
+
  
 
 # Constraints:
 
-# 0 <= nums.length <= 20
-# -231 <= nums[i] <= 231 - 1
-# All the values of nums are unique.
-# nums is sorted in ascending order.
+# 1 <= s.length, t.length <= 5 * 104
+# s and t consist of lowercase English letters.
 
 class Solution:
-    def summaryRanges(self, nums: List[int]) -> List[str]:
-        def f(i: int, j: int) -> str:
-            return str(nums[i]) if i == j else f'{nums[i]}->{nums[j]}'
-
-        i = 0
-        n = len(nums)
-        ans = []
-        while i < n:
-            j = i
-            while j + 1 < n and nums[j + 1] == nums[j] + 1:
-                j += 1
-            ans.append(f(i, j))
-            i = j + 1
-        return ans
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        cnt = Counter(s)
+        for c in t:
+            cnt[c] -= 1
+            if cnt[c] < 0:
+                return False
+        return True
