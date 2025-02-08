bed0ae0abf0073c34a094d7162e333fa7fa93e2c
jenstem <jennifer.stemkowski@gmail.com>
2025-02-08 07:03:00 +0100

    Solve Problem No. 290 Word Pattern

--- leetcode.py
+++ leetcode.py
@@ -1,20 +1,35 @@
-# Valid Anagram #242
+# Word Pattern #290
 
-# Given two strings s and t, return true if t is an 
-# anagram
-#  of s, and false otherwise.
+# Given a pattern and a string s, find if s follows the same pattern.
 
+# Here follow means a full match, such that there is a bijection between a letter in pattern and a non-empty word in s. Specifically:
+
+# Each letter in pattern maps to exactly one unique word in s.
+# Each unique word in s maps to exactly one letter in pattern.
+# No two letters map to the same word, and no two words map to the same letter.
  
 
 # Example 1:
 
-# Input: s = "anagram", t = "nagaram"
+# Input: pattern = "abba", s = "dog cat cat dog"
 
 # Output: true
 
+# Explanation:
+
+# The bijection can be established as:
+
+# 'a' maps to "dog".
+# 'b' maps to "cat".
 # Example 2:
 
-# Input: s = "rat", t = "car"
+# Input: pattern = "abba", s = "dog cat cat fish"
+
+# Output: false
+
+# Example 3:
+
+# Input: pattern = "aaaa", s = "dog cat cat dog"
 
 # Output: false
 
@@ -22,16 +37,23 @@
 
 # Constraints:
 
-# 1 <= s.length, t.length <= 5 * 104
-# s and t consist of lowercase English letters.
+# 1 <= pattern.length <= 300
+# pattern contains only lower-case English letters.
+# 1 <= s.length <= 3000
+# s contains only lowercase English letters and spaces ' '.
+# s does not contain any leading or trailing spaces.
+# All the words in s are separated by a single space.
 
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        ws = s.split()
+        if len(pattern) != len(ws):
             return False
-        cnt = Counter(s)
-        for c in t:
-            cnt[c] -= 1
-            if cnt[c] < 0:
+        d1 = {}
+        d2 = {}
+        for a, b in zip(pattern, ws):
+            if (a in d1 and d1[a] != b) or (b in d2 and d2[b] != a):
                 return False
+            d1[a] = b
+            d2[b] = a
         return True
