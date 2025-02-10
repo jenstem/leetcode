e5d6248ea789850aa74f68891f252f6947166268
jenstem <jennifer.stemkowski@gmail.com>
2025-02-10 07:48:00 +0100

    Solve Problem No. 342 Power of Four

--- leetcode.py
+++ leetcode.py
@@ -1,26 +1,23 @@
-# Power of Three #326
+# Power of Four #342
 
-# Given an integer n, return true if it is a power of three. Otherwise, return false.
+# Given an integer n, return true if it is a power of four. Otherwise, return false.
 
-# An integer n is a power of three, if there exists an integer x such that n == 3x.
+# An integer n is a power of four, if there exists an integer x such that n == 4x.
 
  
 
 # Example 1:
 
-# Input: n = 27
+# Input: n = 16
 # Output: true
-# Explanation: 27 = 33
 # Example 2:
 
-# Input: n = 0
+# Input: n = 5
 # Output: false
-# Explanation: There is no x where 3x = 0.
 # Example 3:
 
-# Input: n = -1
-# Output: false
-# Explanation: There is no x where 3x = (-1).
+# Input: n = 1
+# Output: true
  
 
 # Constraints:
@@ -28,9 +25,9 @@
 # -231 <= n <= 231 - 1
 
 class Solution:
-    def isPowerOfThree(self, n: int) -> bool:
-            while n > 2:
-                if n % 3:
-                    return False
-                n //= 3
-            return n == 1
+    def isPowerOfFour(self, n: int) -> bool:
+        while n > 2:
+            if n % 4:
+                return False
+            n //= 4
+        return n == 1
