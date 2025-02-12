7d5f9206d3a4fb66e828ab254d557aad0f659540
jenstem <jennifer.stemkowski@gmail.com>
2025-02-12 06:19:00 +0100

    Solve Problem No. 338 Counting Bits

--- leetcode.py
+++ leetcode.py
@@ -1,40 +1,34 @@
-# Range Sum Query #303
+# Counting Bits #338
 
-# Given an integer array nums, handle multiple queries of the following type:
+# Given an integer n, return an array ans of length n + 1 such that for each i (0 <= i <= n), ans[i] is the number of 1's in the binary representation of i.
 
-# Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
-# Implement the NumArray class:
-
-# NumArray(int[] nums) Initializes the object with the integer array nums.
-# int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
  
 
 # Example 1:
 
-# Input
-# ["NumArray", "sumRange", "sumRange", "sumRange"]
-# [[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
-# Output
-# [null, 1, -1, -3]
+# Input: n = 2
+# Output: [0,1,1]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# Example 2:
 
-# Explanation
-# NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
-# numArray.sumRange(0, 2); // return (-2) + 0 + 3 = 1
-# numArray.sumRange(2, 5); // return 3 + (-5) + 2 + (-1) = -1
-# numArray.sumRange(0, 5); // return (-2) + 0 + 3 + (-5) + 2 + (-1) = -3
+# Input: n = 5
+# Output: [0,1,1,2,1,2]
+# Explanation:
+# 0 --> 0
+# 1 --> 1
+# 2 --> 10
+# 3 --> 11
+# 4 --> 100
+# 5 --> 101
  
 
 # Constraints:
 
-# 1 <= nums.length <= 104
-# -105 <= nums[i] <= 105
-# 0 <= left <= right < nums.length
-# At most 104 calls will be made to sumRange.
+# 0 <= n <= 105
 
-class NumArray:
-
-    def __init__(self, nums: List[int]):
-        self.s = list(accumulate(nums, initial=0))
-
-    def sumRange(self, left: int, right: int) -> int:
-        return self.s[right + 1] - self.s[left]
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        return [i.bit_count() for i in range(n + 1)]
