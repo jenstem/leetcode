bb6688f7b974dd162e9a71e9424ecb437e78da5f
jenstem <jennifer.stemkowski@gmail.com>
2025-03-02 14:58:00 +0100

    Solve Problem No. 404 Sum of Left Leaves

--- leetcode.py
+++ leetcode.py
@@ -1,41 +1,37 @@
-# First Unique Character in a String #387
+# Sum of Left Leaves #404
 
-# Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+# Given the root of a binary tree, return the sum of all left leaves.
 
+# A leaf is a node with no children. A left leaf is a leaf that is the left child of another node.
 
 # Example 1:
 
-# Input: s = "leetcode"
-
-# Output: 0
-
-# Explanation:
-
-# The character 'l' at index 0 is the first character that does not occur at any other index.
-
+# Input: root = [3,9,20,null,null,15,7]
+# Output: 24
+# Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
 # Example 2:
 
-# Input: s = "loveleetcode"
-
-# Output: 2
-
-# Example 3:
-
-# Input: s = "aabb"
-
-# Output: -1
-
-
-
+# Input: root = [1]
+# Output: 0
+ 
 # Constraints:
+# The number of nodes in the tree is in the range [1, 1000].
+# -1000 <= Node.val <= 1000
 
-# 1 <= s.length <= 105
-# s consists of only lowercase English letters.
-
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def firstUniqChar(self, s: str) -> int:
-        cnt = Counter(s)
-        for i, c in enumerate(s):
-            if cnt[c] == 1:
-                return i
-        return -1
+    def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        if root is None:
+            return 0
+        ans = self.sumOfLeftLeaves(root.right)
+        if root.left:
+            if root.left.left == root.left.right:
+                ans += root.left.val
+            else:
+                ans += self.sumOfLeftLeaves(root.left)
+        return ans
