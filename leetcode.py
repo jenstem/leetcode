3bb8d547dc96ff973c3ce2f668668944b25a745b
jenstem <jennifer.stemkowski@gmail.com>
2025-04-03 07:02:00 +0100

    Solve Problem No. 2765 Longest Alternating Array

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,23 @@
-# Valid Mountain Array #941
+# Longest Alternating Array #2765
 
-# Given an array of integers arr, return true if and only if it is a valid mountain array.
+# You are given a 0-indexed integer array nums. A subarray s of length m is called alternating if:
 
-# Recall that arr is a mountain array if and only if:
+# m is greater than 1.
+# s1 = s0 + 1.
+# The 0-indexed subarray s looks like [s0, s1, s0, s1,...,s(m-1) % 2]. In other words, s1 - s0 = 1, s2 - s1 = -1, s3 - s2 = 1, s4 - s3 = -1, and so on up to s[m - 1] - s[m - 2] = (-1)m.
+# Return the maximum length of all alternating subarrays present in nums or -1 if no such subarray exists.
 
-# arr.length >= 3
-# There exists some i with 0 < i < arr.length - 1 such that:
-# arr[0] < arr[1] < ... < arr[i - 1] < arr[i] 
-# arr[i] > arr[i + 1] > ... > arr[arr.length - 1]
- 
+# A subarray is a contiguous non-empty sequence of elements within an array.
 
 class Solution:
-    def validMountainArray(self, arr: List[int]) -> bool:
-        n = len(arr)
-        if n < 3:
-            return False
-        i, j = 0, n - 1
-        while i + 1 < n - 1 and arr[i] < arr[i + 1]:
-            i += 1
-        while j - 1 > 0 and arr[j - 1] > arr[j]:
-            j -= 1
-        return i == j
+    def alternatingSubarray(self, nums: List[int]) -> int:
+        ans, n = -1, len(nums)
+        for i in range(n):
+            k = 1
+            j = i
+            while j + 1 < n and nums[j + 1] - nums[j] == k:
+                j += 1
+                k *= -1
+            if j - i + 1 > 1:
+                ans = max(ans, j - i + 1)
+        return ans
