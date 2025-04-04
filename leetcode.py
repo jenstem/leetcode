40be35d3ba4d31f8b773a5be0248a28cfef2f608
jenstem <jennifer.stemkowski@gmail.com>
2025-04-04 07:39:00 +0100

    Solve Problem No. 2660 Determine the Winner of a Bowling Game

--- leetcode.py
+++ leetcode.py
@@ -1,32 +1,41 @@
-# Tenth Line #195
+# Determine the Winner of a Bowling Game #2660
 
-# Given a text file file.txt, print just the 10th line of the file.
+# You are given two 0-indexed integer arrays player1 and player2, representing the number of pins that player 1 and player 2 hit in a bowling game, respectively.
 
-# Example:
+# The bowling game consists of n turns, and the number of pins in each turn is exactly 10.
 
-# Assume that file.txt has the following content:
+# Assume a player hits xi pins in the ith turn. The value of the ith turn for the player is:
 
-# Line 1
-# Line 2
-# Line 3
-# Line 4
-# Line 5
-# Line 6
-# Line 7
-# Line 8
-# Line 9
-# Line 10
-# Your script should output the tenth line, which is:
+# 2xi if the player hits 10 pins in either (i - 1)th or (i - 2)th turn.
+# Otherwise, it is xi.
+# The score of the player is the sum of the values of their n turns.
 
-# Line 10
-# Note:
-# 1. If the file contains less than 10 lines, what should you output?
-# 2. There's at least three different solutions. Try to explore all possibilities.
+# Return
 
-# Read from the file file.txt and output the tenth line to stdout.
-with open('file.txt', 'r') as file:
-    lines = file.readlines()
-    if len(lines) >= 10:
-        print(lines[9].strip())
-    else:
-        print("The file contains less than 10 lines.")
+# 1 if the score of player 1 is more than the score of player 2,
+# 2 if the score of player 2 is more than the score of player 1, and
+# 0 in case of a draw.
+
+class Solution:
+    def isWinner(self, player1: List[int], player2: List[int]) -> int:
+        def calculate_score(player: List[int]) -> int:
+            score = 0
+            n = len(player)
+            for i in range(n):
+                if i > 1 and (player[i - 1] == 10 or player[i - 2] == 10):
+                    score += 2 * player[i]
+                elif i > 0 and player[i - 1] == 10:
+                    score += 2 * player[i]
+                else:
+                    score += player[i]
+            return score
+        
+        score1 = calculate_score(player1)
+        score2 = calculate_score(player2)
+        
+        if score1 > score2:
+            return 1
+        elif score2 > score1:
+            return 2
+        else:
+            return 0
