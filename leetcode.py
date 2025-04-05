a41699929f9e789e87fb17cbdb00c27387ee11a9
jenstem <jennifer.stemkowski@gmail.com>
2025-04-05 06:53:00 +0100

    Solve Problem No. 3456 Find Special Substring of Length K

--- leetcode.py
+++ leetcode.py
@@ -1,41 +1,22 @@
-# Determine the Winner of a Bowling Game #2660
+# Find Special Substring of Length K #3456
 
-# You are given two 0-indexed integer arrays player1 and player2, representing the number of pins that player 1 and player 2 hit in a bowling game, respectively.
+# You are given a string s and an integer k.
 
-# The bowling game consists of n turns, and the number of pins in each turn is exactly 10.
+# Determine if there exists a substring of length exactly k in s that satisfies the following conditions:
 
-# Assume a player hits xi pins in the ith turn. The value of the ith turn for the player is:
-
-# 2xi if the player hits 10 pins in either (i - 1)th or (i - 2)th turn.
-# Otherwise, it is xi.
-# The score of the player is the sum of the values of their n turns.
-
-# Return
-
-# 1 if the score of player 1 is more than the score of player 2,
-# 2 if the score of player 2 is more than the score of player 1, and
-# 0 in case of a draw.
+# The substring consists of only one distinct character (e.g., "aaa" or "bbb").
+# If there is a character immediately before the substring, it must be different from the character in the substring.
+# If there is a character immediately after the substring, it must also be different from the character in the substring.
+# Return true if such a substring exists. Otherwise, return false.
 
 class Solution:
-    def isWinner(self, player1: List[int], player2: List[int]) -> int:
-        def calculate_score(player: List[int]) -> int:
-            score = 0
-            n = len(player)
-            for i in range(n):
-                if i > 1 and (player[i - 1] == 10 or player[i - 2] == 10):
-                    score += 2 * player[i]
-                elif i > 0 and player[i - 1] == 10:
-                    score += 2 * player[i]
-                else:
-                    score += player[i]
-            return score
-        
-        score1 = calculate_score(player1)
-        score2 = calculate_score(player2)
-        
-        if score1 > score2:
-            return 1
-        elif score2 > score1:
-            return 2
-        else:
-            return 0
+    def hasSpecialSubstring(self, s: str, k: int) -> bool:
+        l, n = 0, len(s)
+        while l < n:
+            r = l
+            while r < n and s[r] == s[l]:
+                r += 1
+            if r - l == k:
+                return True
+            l = r
+        return False
