c262559ce2be6cffda845b3cfedb2d3fcfff2adb
jenstem <jennifer.stemkowski@gmail.com>
2025-04-06 14:21:00 +0100

    Solve Problem No. 3114 Latest Time You Can Obtain After Replacing Characters

--- leetcode.py
+++ leetcode.py
@@ -1,22 +1,18 @@
-# Find Special Substring of Length K #3456
+# Latest Time You Can Obtain After Replacing Characters #3114
 
-# You are given a string s and an integer k.
 
-# Determine if there exists a substring of length exactly k in s that satisfies the following conditions:
+# You are given a string s representing a 12-hour format time where some of the digits (possibly none) are replaced with a "?".
 
-# The substring consists of only one distinct character (e.g., "aaa" or "bbb").
-# If there is a character immediately before the substring, it must be different from the character in the substring.
-# If there is a character immediately after the substring, it must also be different from the character in the substring.
-# Return true if such a substring exists. Otherwise, return false.
+# 12-hour times are formatted as "HH:MM", where HH is between 00 and 11, and MM is between 00 and 59. The earliest 12-hour time is 00:00, and the latest is 11:59.
+
+# You have to replace all the "?" characters in s with digits such that the time we obtain by the resulting string is a valid 12-hour format time and is the latest possible.
+
+# Return the resulting string.
 
 class Solution:
-    def hasSpecialSubstring(self, s: str, k: int) -> bool:
-        l, n = 0, len(s)
-        while l < n:
-            r = l
-            while r < n and s[r] == s[l]:
-                r += 1
-            if r - l == k:
-                return True
-            l = r
-        return False
+    def findLatestTime(self, s: str) -> str:
+        for h in range(11, -1, -1):
+            for m in range(59, -1, -1):
+                t = f"{h:02d}:{m:02d}"
+                if all(a == b for a, b in zip(s, t) if a != "?"):
+                    return t
