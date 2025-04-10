1b766f280a75605faf3fee9f1195e4077dadc9e2
jenstem <jennifer.stemkowski@gmail.com>
2025-04-10 10:01:27 -0400

    Solve Problem No. 3264 Final Array State After K Multiplication Operations 1

--- leetcode.py
+++ leetcode.py
@@ -1,18 +1,17 @@
-# Latest Time You Can Obtain After Replacing Characters #3114
+# Final Array State After K Multiplication Operations 1 #3264
 
 
-# You are given a string s representing a 12-hour format time where some of the digits (possibly none) are replaced with a "?".
+# You are given an integer array nums, an integer k, and an integer multiplier.
 
-# 12-hour times are formatted as "HH:MM", where HH is between 00 and 11, and MM is between 00 and 59. The earliest 12-hour time is 00:00, and the latest is 11:59.
+# You need to perform k operations on nums. In each operation:
 
-# You have to replace all the "?" characters in s with digits such that the time we obtain by the resulting string is a valid 12-hour format time and is the latest possible.
-
-# Return the resulting string.
+# Find the minimum value x in nums. If there are multiple occurrences of the minimum value, select the one that appears first.
+# Replace the selected minimum value x with x * multiplier.
+# Return an integer array denoting the final state of nums after performing all k operations.
 
 class Solution:
-    def findLatestTime(self, s: str) -> str:
-        for h in range(11, -1, -1):
-            for m in range(59, -1, -1):
-                t = f"{h:02d}:{m:02d}"
-                if all(a == b for a, b in zip(s, t) if a != "?"):
-                    return t
+    def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
+            for _ in range(k):
+                min_index = nums.index(min(nums))
+                nums[min_index] = nums[min_index] * multiplier
+            return nums  
