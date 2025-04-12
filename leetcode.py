9ced6e406379c8719a61698639749cd51849e2e5
jenstem <jennifer.stemkowski@gmail.com>
2025-04-12 06:48:00 +0100

    Solve Problem No. 441 Arranging Coins

--- leetcode.py
+++ leetcode.py
@@ -1,16 +1,16 @@
-# Check If N and Its Double Exist #1346
+# Arranging Coins #441
 
-# Given an array arr of integers, check if there exist two indices i and j such that :
+# You have n coins and you want to build a staircase with these coins. The staircase consists of k rows where the ith row has exactly i coins. The last row of the staircase may be incomplete.
 
-# i != j
-# 0 <= i, j < arr.length
-# arr[i] == 2 * arr[j]
+# Given the integer n, return the number of complete rows of the staircase you will build.
 
 class Solution:
-    def checkIfExist(self, arr: List[int]) -> bool:
-        s = set()
-        for x in arr:
-            if x * 2 in s or (x % 2 == 0 and x // 2 in s):
-                return True
-            s.add(x)
-        return False
+    def arrangeCoins(self, n: int) -> int:
+        left, right = 0, n
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * (mid + 1) // 2 <= n:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
