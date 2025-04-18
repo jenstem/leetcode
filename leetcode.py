3ca573924fd82c9de8d52d9a96cb6ff7d8474c8c
jenstem <jennifer.stemkowski@gmail.com>
2025-04-18 06:42:00 +0100

    Solve Problem No. 2614 Prime in Diagonal

--- leetcode.py
+++ leetcode.py
@@ -1,14 +1,26 @@
-# Maximum Strong Pair XOR I #2932
+# Prime in Diagonal #2614
 
-# You are given a 0-indexed integer array nums. A pair of integers x and y is called a strong pair if it satisfies the condition:
+# You are given a 0-indexed two-dimensional integer array nums.
 
-# |x - y| <= min(x, y)
-# You need to select two integers from nums such that they form a strong pair and their bitwise XOR is the maximum among all strong pairs in the array.
+# Return the largest prime number that lies on at least one of the diagonals of nums. In case, no prime is present on any of the diagonals, return 0.
 
-# Return the maximum XOR value out of all possible strong pairs in the array nums.
+# Note that:
 
-# Note that you can pick the same integer twice to form a pair.
+# An integer is prime if it is greater than 1 and has no positive integer divisors other than 1 and itself.
+# An integer val is on one of the diagonals of nums if there exists an integer i for which nums[i][i] = val or an i for which nums[i][nums.length - i - 1] = val.
 
 class Solution:
-    def maximumStrongPairXor(self, nums: List[int]) -> int:
-        return max(x ^ y for x in nums for y in nums if abs(x - y) <= min(x, y))
+    def diagonalPrime(self, nums: List[List[int]]) -> int:
+        def is_prime(x: int) -> bool:
+            if x < 2:
+                return False
+            return all(x % i for i in range(2, int(sqrt(x)) + 1))
+
+        n = len(nums)
+        ans = 0
+        for i, row in enumerate(nums):
+            if is_prime(row[i]):
+                ans = max(ans, row[i])
+            if is_prime(row[n - i - 1]):
+                ans = max(ans, row[n - i - 1])
+        return ans
