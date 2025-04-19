689fbe365a29f284d15f2da2dd1f28f529416630
jenstem <jennifer.stemkowski@gmail.com>
2025-04-19 06:28:00 +0100

    Solve Problem No. 434 Number of Segments in a String

--- leetcode.py
+++ leetcode.py
@@ -1,26 +1,21 @@
-# Prime in Diagonal #2614
+# Number of Segments in a String #434
 
-# You are given a 0-indexed two-dimensional integer array nums.
+# Given a string s, return the number of segments in the string.
 
-# Return the largest prime number that lies on at least one of the diagonals of nums. In case, no prime is present on any of the diagonals, return 0.
+# A segment is defined to be a contiguous sequence of non-space characters.
 
-# Note that:
+ 
 
-# An integer is prime if it is greater than 1 and has no positive integer divisors other than 1 and itself.
-# An integer val is on one of the diagonals of nums if there exists an integer i for which nums[i][i] = val or an i for which nums[i][nums.length - i - 1] = val.
+# Example 1:
+
+# Input: s = "Hello, my name is John"
+# Output: 5
+# Explanation: The five segments are ["Hello,", "my", "name", "is", "John"]
+# Example 2:
+
+# Input: s = "Hello"
+# Output: 1
 
 class Solution:
-    def diagonalPrime(self, nums: List[List[int]]) -> int:
-        def is_prime(x: int) -> bool:
-            if x < 2:
-                return False
-            return all(x % i for i in range(2, int(sqrt(x)) + 1))
-
-        n = len(nums)
-        ans = 0
-        for i, row in enumerate(nums):
-            if is_prime(row[i]):
-                ans = max(ans, row[i])
-            if is_prime(row[n - i - 1]):
-                ans = max(ans, row[n - i - 1])
-        return ans
+    def countSegments(self, s: str) -> int:
+        return len(s.split())
