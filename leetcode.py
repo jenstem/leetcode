359902cfdbdbf8ebbd53ef059b73ad6ff9ae247b
jenstem <jennifer.stemkowski@gmail.com>
2025-04-23 15:22:51 -0400

    Solve Problem No. 2032 Two out of three

--- leetcode.py
+++ leetcode.py
@@ -1,10 +1,10 @@
-# Check if All Characters have Equal Number of Occurrences #1941
+# Two Out of Three #2032
 
-# Given a string s, return true if s is a good string, or false otherwise.
-
-# A string s is good if all the characters that appear in s have the same number of occurrences (i.e., the same frequency).
+# Given three integer arrays nums1, nums2, and nums3, return a distinct array 
+# containing all the values that are present in at least two out of the three 
+# arrays. You may return the values in any order.
 
 class Solution:
-    def areOccurrencesEqual(self, s: str) -> bool:
-        return len(set(Counter(s).values())) == 1
-
+    def twoOutOfThree(self, nums1: List[int], nums2: List[int], nums3: List[int]) -> List[int]:
+        s1, s2, s3 = set(nums1), set(nums2), set(nums3)
+        return [i for i in range(1, 101) if (i in s1) + (i in s2) + (i in s3) > 1]
