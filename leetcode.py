77260ae8a2c8de461d71cab5fe9d6567a6a24b7e
jenstem <jennifer.stemkowski@gmail.com>
2025-04-26 07:13:00 +0100

    Solve Problem No. 12 Integer to Roman

--- leetcode.py
+++ leetcode.py
@@ -1,23 +1,30 @@
-# Container with Most Water #11
+# Integer to Roman #12
 
-# You are given an integer array height of length n. There are n vertical lines drawn such that the two endpoints of the ith line are (i, 0) and (i, height[i]).
+# Seven different symbols represent Roman numerals with the following values:
 
-# Find two lines that together with the x-axis form a container, such that the container contains the most water.
+# Symbol	Value
+# I	1
+# V	5
+# X	10
+# L	50
+# C	100
+# D	500
+# M	1000
+# Roman numerals are formed by appending the conversions of decimal place values from highest to lowest. Converting a decimal place value into a Roman numeral has the following rules:
 
-# Return the maximum amount of water a container can store.
-
-# Notice that you may not slant the container.
+# If the value does not start with 4 or 9, select the symbol of the maximal value that can be subtracted from the input, append that symbol to the result, subtract its value, and convert the remainder to a Roman numeral.
+# If the value starts with 4 or 9 use the subtractive form representing one symbol subtracted from the following symbol, for example, 4 is 1 (I) less than 5 (V): IV and 9 is 1 (I) less than 10 (X): IX. Only the following subtractive forms are used: 4 (IV), 9 (IX), 40 (XL), 90 (XC), 400 (CD) and 900 (CM).
+# Only powers of 10 (I, X, C, M) can be appended consecutively at most 3 times to represent multiples of 10. You cannot append 5 (V), 50 (L), or 500 (D) multiple times. If you need to append a symbol 4 times use the subtractive form.
+# Given an integer, convert it to a Roman numeral.
 
 class Solution:
-    def maxArea(self, height: List[int]) -> int:
-        l, r = 0, len(height) - 1
-        ans = 0
-        while l < r:
-            t = min(height[l], height[r]) * (r - l)
-            ans = max(ans, t)
-            if height[l] < height[r]:
-                l += 1
-            else:
-                r -= 1
-        return ans
+    def intToRoman(self, num: int) -> str:
+        cs = ('M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I')
+        vs = (1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1)
+        ans = []
+        for c, v in zip(cs, vs):
+            while num >= v:
+                num -= v
+                ans.append(c)
+        return ''.join(ans)
 
